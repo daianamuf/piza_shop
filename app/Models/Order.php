28430c84db4c3dcc75903c20a4932e69913d9881
Daiana <muflicdaiana@gmail.com.>
2025-01-15 11:34:12 +0200

    first commit

---
+++ app/Models/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public function pizzas()
+    {
+        return $this->belongsToMany(Pizza::class, 'order_pizzas')->withPivot('quantity');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
